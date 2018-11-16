1ee3968f29c19d77ceebe55df7be4c52476374de
Milosz Blasiak <milblasiak@gmail.com>
2018-11-16 19:13:24 +0100

    Adds histogram equalization and improves image and plots displays
    Ads propper histogram equalizations
    Adds more plots,plots titles group them in bigger plots

--- Histograms/HistogramAdjustmentAndEqualization.m
+++ Histograms/HistogramAdjustmentAndEqualization.m
@@ -1,6 +1,6 @@
 clc;
 clear;
-
+%Show same image with diffrently placed hisogram
 im_1=imread('lena1.bmp');
 im_2=imread('lena2.bmp');
 im_3=imread('lena3.bmp');
@@ -30,22 +30,29 @@ subplot(y,x,7);
 imshow(im_4);
 subplot(y,x,8);
 imhist(im_4,grayScale)
+ 
 
+%Show diffrence of orginal image and image with adjusted histogram
 y=2;
 x=2;
 figure(2);
 subplot(y,x,1);
 hist_1=imread('hist1.bmp');
 imshow(hist_1);
+title("Orginal Image");
 subplot(y,x,2);
 imhist(hist_1);
+title("Orginal Histogram");
 
 subplot(y,x,3);
 im_adjusted=imadjust(hist_1);
 imshow(im_adjusted);
+title("Adjusted Image");
 subplot(y,x,4);
 imhist(im_adjusted);
+title("Adjusted Histogram");
 %%
+%Show accumulated histogram shape on histogram
 [H,v]=imhist(hist_1,256);
 cumulatedH=cumsum(H);
 
@@ -58,9 +65,11 @@ hold on;
 plot(v,H);
 
 plot(v,cumumlatedScaled);
+title("Accumulated Histogram on Histogram plot (Orginal Image)"); 
 %%
+%%Show accumulated histogram shape of adjusted histogram one on another
 figure(4);
-[H,v]=imhist(im_justed,256);
+[H,v]=imhist(im_adjusted,256);
 cumulatedH=cumsum(H);
 maxH=max(H);
 maxK=max(cumulatedH);
@@ -68,6 +77,7 @@ cumumlatedScaled=cumulatedH*maxH/maxK;
 hold on;
 plot(v,H);
 plot(v,cumumlatedScaled);
+title("Accumulated Histogram on Histogram plot (Adjusted Image)"); 
 %%
 [H,v]=imhist(hist_1,256);
 cumulatedH=cumsum(H);
@@ -75,12 +85,43 @@ cumulatedH=cumsum(H);
 maxH=max(H);
 maxK=max(cumulatedH);
 
-
 grayScale=256;
-equalizedHistogram=uint8(round(cumulatedH/maxK*grayScale));
+equalizedHistogramLUT=uint8(round((cumulatedH)/maxK*grayScale));
+equaHist=diff(equalizedHistogramLUT);
 
-plot(v,equalizedHistogram);
+figure(5);
+plot(v,equalizedHistogramLUT);
+title("Equalization LUT function plot ");
+
+%%
+figure(6);
+subplot(2,2,1);
+imshow(hist_1);
+title("Orginal Image");
+subplot(2,2,2);
 
 
-equlizedim=intlut(hist_1,equalizedHistogram);
-imshow(equlizedim);
+[OH,Ov]=imhist(hist_1);
+cummulatedOrginalH=cumsum(OH);
+cummulatedOrginalHScaled=cummulatedOrginalH*max(OH)/max(cummulatedOrginalH);
+
+
+hold on;
+plot(Ov,OH);
+plot(Ov,cummulatedOrginalHScaled);
+title("Orginal Hist + Accumulated Hist");
+
+equalizedIm=intlut(hist_1,equalizedHistogramLUT);
+[H,v]=imhist(equalizedIm,256);
+subplot(2,2,3);
+imshow(equalizedIm);
+title("Image after Equlization");
+cummulatedHist=cumsum(H);
+maxCum=max(cummulatedHist);
+maxHist=max(H);
+cummulatedHistScaled=cummulatedHist*maxHist/maxCum;
+subplot(2,2,4);
+hold on;
+plot(v,cummulatedHistScaled);
+plot(v,H);
+title("Hist and Accumulated Hist after Aqualization");

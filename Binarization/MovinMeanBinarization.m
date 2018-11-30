3a2705c097f2f18c73cc097de87198fb4558311d
Milosz Blasiak <milblasiak@gmail.com>
2018-11-30 14:52:56 +0100

    Correct displaying to actually use implementation

--- Binarization/MovinMeanBinarization.m
+++ Binarization/MovinMeanBinarization.m
@@ -15,16 +15,16 @@ hist=zeros(1,20);
 my_mean=0;
 
 
-for x=20:X
+for x=1:X
     for y=1:Y   
+    
         my_mean=my_mean+(img(x,y)/N)-(hist(20)/N);
-        
         hist=[img(x,y),hist(1:19)];
         
         tresholold=my_mean*a;
         
         if(img(x,y)>tresholold)
-            img_bin(x,y)=1;
+            img_bin(x,y)=255;
             
         else
             img_bin(x,y)=0;
@@ -32,7 +32,8 @@ for x=20:X
         
     end
 end
+
 subplot(1,2,1);
 imshow(img);
 subplot(1,2,2);
-imshow(boolean(img_bin));
+imshow(img_bin);

2ae55556e2a8fb0908ee82c29464753ebeb15805
Milosz Blasiak <milblasiak@gmail.com>
2018-12-04 15:29:48 +0100

    Show gaussian filter efect

--- Filters/Filtration.m
+++ Filters/Filtration.m
@@ -6,15 +6,20 @@ img=imread('plansza.bmp');
 
 X=3;
 Y=1;
-winSize=[3,5,6,15];
+
+
 
 my_mask=[1,2,1;2,4,2;1,2,1;]
 my_mask=my_mask/sum(my_mask);
-for x=1:4
+
+winSize=[3,5,9,15,35];
+
+for x=1:5
     figure(x);
     k=1;
     
     mask=fspecial('average',winSize(x));
+    %Change mask here mask/my_mask
     img_filtered=conv2(img,my_mask,'same');
 
     
@@ -25,7 +30,7 @@ for x=1:4
     k=k+1;
     subplot(Y,X,k);
     imshow(uint8(img_filtered));
-    title("Filtered ");
+    title(["Filtered with widow",winSize(x),"x",winSize(x)]);
     k=k+1;
     
     subplot(Y,X,k);
@@ -40,8 +45,35 @@ end
 %Gausian Mask
 
 
-mask_gaus=fspecial('gaussian',0.5);
 
-mesh(mask_gaus);
+mask_gaus=fspecial('gaussian',5,0.5);
+%mesh(mask_gaus);
+img_gaus_half=conv2(img,mask_gaus,'same');
+
+mask_gaus=fspecial('gaussian',5,1);
+%mesh(mask_gaus);
+img_gaus_one=conv2(img,mask_gaus,'same');
+
+mask_gaus=fspecial('gaussian',5,0.1);
+%mesh(mask_gaus);
+img_gaus_two=conv2(img,mask_gaus,'same');
 
 
+
+
+
+subplot(2,2,1);
+imshow(img);
+title("orginal");
+
+subplot(2,2,2);
+imshow(img_gaus_half);
+title("Gause 1/2");
+
+subplot(2,2,3);
+imshow(img_gaus_one);
+title("Gaus 1");
+
+subplot(2,2,4);
+imshow(img_gaus_two);
+title("Gaus 1/10");

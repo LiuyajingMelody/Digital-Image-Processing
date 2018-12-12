4b52d88e6199d804f29c6a0cb10fe45b14106c78
Milosz Blasiak <milblasiak@gmail.com>
2018-12-12 12:38:59 +0100

    Create Bilateral filtration core functions

---
+++ BilateralFiltration/convolution_local.m
@@ -0,0 +1,12 @@
+function [outputArg1,outputArg2] = convolution_local(data_,local_window_)
+Nx=size(data_(2));
+h=1;
+for i=1:Nx
+    
+    patch=reshape 
+    data_filtered(i)=sum(sum(patch.*h));
+    
+
+
+end
+

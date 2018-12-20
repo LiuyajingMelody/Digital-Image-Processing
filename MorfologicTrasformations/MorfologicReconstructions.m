63135ee10e100b0261daf2ec87647fa7c8af1d78
Milosz Blasiak <milblasiak@gmail.com>
2018-12-20 11:38:28 +0100

    Add Edge Cleaning showcase

--- MorfologicTrasformations/MorfologicReconstructions.m
+++ MorfologicTrasformations/MorfologicReconstructions.m
@@ -10,7 +10,7 @@ img_detect=imopen(img,SE);
 
 img_rec=imreconstruct(img_detect,img);
 
-
+img_clean=imclearborder(img_rec);
 
 
 
@@ -32,6 +32,12 @@ subplot(2,2,3);
 imshow(img_rec);
 title('Reconstructed using imrec with img as mask');
 
+
+subplot(2,2,4);
+imshow(img_clean);
+title('Cleaned');
+
+
 %%
 %Img filling
 
@@ -48,6 +54,23 @@ title('Orginal');
 subplot(1,2,2);
 imshow(img_fill);
 title('Filled using holes filling');
+
+
+%%
+%Boreder cleaning
+
+img=imread('text.bmp');
+img_clear=imclearborder(img);
+figure(3);
+subplot(1,2,1);
+imshow(img);
+title('Orginal');
+
+
+subplot(1,2,2);
+imshow(img_clear);
+title('Cleaned Borders');
+
 
 
 

67620840ed5ee774b1942a967ebac4c7b80509c5
Milosz Blasiak <milblasiak@gmail.com>
2018-12-20 01:17:30 +0100

    Add showcasing thinning and skeletization

--- MorfologicTrasformations/Thinning.m
+++ MorfologicTrasformations/Thinning.m
@@ -1,3 +1,46 @@
 close all;
 clc;
 clear;
+
+%Thinning
+
+figure(1);
+subplot(2,2,1);
+img=imread('fingerprint.bmp');
+imshow(img);
+title('Orginal');
+
+subplot(2,2,2);
+img_mod=bwmorph(img,'thin',1);
+imshow(img_mod);
+title('Thined times=1');
+
+
+
+subplot(2,2,3);
+img_mod=bwmorph(img,'thin',2);
+imshow(img_mod);
+title('Thined times=2');
+
+
+
+subplot(2,2,4);
+img_mod=bwmorph(img,'thin',Inf);
+imshow(img_mod);
+title('Thined times=Inf');
+%%
+%Skeletization
+
+
+img=imread('kosc.bmp');
+
+figure(2);
+subplot(1,2,1);
+imshow(img);
+title('Orginal');
+
+img_mod=bwmorph(img,'skel',Inf);
+
+subplot(1,2,2)
+imshow(img_mod);
+title('Skeletonization Inf time');

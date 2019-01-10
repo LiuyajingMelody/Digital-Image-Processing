524cf003963346510a2bfe6f3bf6c237efeaf665
Milosz Blasiak <milblasiak@gmail.com>
2019-01-10 01:11:03 +0100

    Create expanding segmenation base

---
+++ Segmentation/ExpandingSegmentation.m
@@ -0,0 +1,67 @@
+clc;
+close all;
+clear;
+
+
+knee=double(imread('knee.png'));
+
+%imshow(knee,[]);
+%selected=ginput(1);
+selected=[277,217];
+
+
+
+[Y X]=size(knee);
+visited=zeros(Y,X);
+segmented=zeros(Y,X);
+
+
+stacki=zeros(1000,2);
+ptr=1;
+
+visited(selected(1),selected(2))=1;
+
+segmented(selected(1),selected(2))=1;
+stacki(ptr,:)=[selected(1),selected(2)];
+
+win_size=1;
+treshold=4;
+
+
+while(ptr>0)
+   
+   w_y=stacki(ptr,1);
+   w_x=stacki(ptr,1);
+   stacki(ptr,:)=[0,0];
+   pre=ptr-1;
+   
+   
+   if(Y-win_size>w_y>win_size && win_size<w_x<X-win_size)
+       center=knee(w_y,w_x);
+       
+       for y=(w_y-win_size):(w_y+win_size)
+          for x=(w_x-win_size):(w_x+win_size)
+    
+            if(visited(w_y,w_x)==0)
+                current=knee(w_y,w_x);
+                if(abs(center-current)<=treshold)
+                    
+                
+                
+                end
+            end
+    
+          end
+       end
+       
+       
+       
+   end
+    
+    
+    
+    
+    
+    
+    
+end
